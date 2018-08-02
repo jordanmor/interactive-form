f0a6709e73aac1189676d3270579530ec9726dac
Jordan Mor <jordanmor@gmail.com>
2018-08-02 19:49:46 -0400

    Add event listener to toggle Other Job option

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-$name = $('#name').focus();
+/*=============-=============-=============-=============
+                    UNOBTRUSIVE JAVASCRIPT
+===============-=============-=============-===========*/
+
+/*=============-=============-=============-=============
+                      CACHED VARIABLES
+===============-=============-=============-===========*/
+
+$name = $('#name').focus(); // Set focus on the first text field
+
+// Job Role Section
+const $otherTitle = $('#other-title').addClass('is-hidden');
+
+/*=============-=============-=============-=============
+                        FUNCTIONS
+===============-=============-=============-===========*/
+
+
+/*=============-=============-=============-=============
+                    EVENT LISTENERS
+===============-=============-=============-===========*/
+
+// Job Role Section
+$('#title').on('change', function() {
+  if(this.value === 'other') {
+    $otherTitle.removeClass('is-hidden');
+  } else {
+    $otherTitle.addClass('is-hidden');
+  }
+});
