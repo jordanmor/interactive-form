6e0743538043d3624ad9d0e87ad1ab4474827555
Jordan Mor <jordanmor@gmail.com>
2018-08-05 19:16:44 -0400

    Avoid scheduling conflicts when registering for activities

--- js/app.js
+++ js/app.js
@@ -10,14 +10,39 @@ const $name = $('#name').focus(); // Set focus on the first text field
 
 // Job Role Section
 const $otherTitle = $('#other-title').addClass('is-hidden');
+
 // T-Shirt Info Section
 const $colorOptions = $('#color option');
 const $tShirtColorsDiv = $('#colors-js-puns').hide(); //Color menu hidden until T-Shirt design selected
+
+// Register for Activities Section
+$activitiesInputs = $('.activities input');
+$express = $('.activities input[name="express"]');
+$jsFramework = $('.activities input[name="js-frameworks"]');
+$jsLibs = $('.activities input[name="js-libs"]');
+$node = $('.activities input[name="node"]');
 
 /*=============-=============-=============-=============
                         FUNCTIONS
 ===============-=============-=============-===========*/
 
+// Register for Activities Section
+function avoidSchedulingConflicts(workshop, conflictingWorkshop) {
+  if(workshop.prop('checked')) {
+      conflictingWorkshop
+        .prop('disabled', true)
+        .parent()
+        .append('<span class="unavailable">&nbsp;&nbsp;Unavailable</span>')
+        .css('color', '#cbcbcb');
+  } else {
+      conflictingWorkshop
+        .prop('disabled', false)
+        .parent()
+        .css('color', '#000')
+        .children('.unavailable')
+        .remove();
+  }
+}
 
 /*=============-=============-=============-=============
                     EVENT LISTENERS
@@ -35,19 +60,30 @@ $('#title').on('change', function() {
 // T-Shirt Info Section
 $('#design').on('change', function() {
   if(this.value === 'js puns') {
-      $tShirtColorsDiv.show();
-      $colorOptions.prop('selected', false) // no options selected so first 'JS Puns' option will appear
-                   .hide()
-                   .filter((i, option) => option.textContent.includes('JS Puns'))
-                   .show();
+    $tShirtColorsDiv.show();
+    $colorOptions
+      .prop('selected', false) // no options selected so first 'JS Puns' option will appear
+      .hide()
+      .filter((i, option) => option.textContent.includes('JS Puns'))
+      .show();
   } else if(this.value === 'heart js') {
       $tShirtColorsDiv.show();
-      $colorOptions.hide()
-                   .filter((i, option) => option.textContent.includes('I ♥ JS'))
-                   .show()
-                   .first()
-                   .prop('selected', true); // the first filtered 'I ♥ JS' option selected 
+      $colorOptions
+        .hide()
+        .filter((i, option) => option.textContent.includes('I ♥ JS'))
+        .show()
+        .first()
+        .prop('selected', true); // the first filtered 'I ♥ JS' option selected 
   } else {
       $tShirtColorsDiv.hide(); //Color menu hidden if no theme selected
   }
+});
+
+// Register for Activities Section
+$activitiesInputs.on('change', function() {
+  // avoid scheduling conflicts
+  if(this.name === 'js-frameworks') avoidSchedulingConflicts($jsFramework, $express);
+  if(this.name === 'express') avoidSchedulingConflicts($express, $jsFramework);
+  if(this.name === 'js-libs') avoidSchedulingConflicts($jsLibs, $node);
+  if(this.name === 'node') avoidSchedulingConflicts($node, $jsLibs);
 });
