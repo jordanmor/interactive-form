025c078cb7440ed2907785540d5c79b1be1ed59b
Jordan Mor <jordanmor@gmail.com>
2018-08-22 07:30:13 -0400

    Refactor error messaging in form

--- js/script.js
+++ js/script.js
@@ -7,16 +7,7 @@ $('.cc-option').prop('selected', true); // CC payment option selected by default
 
 // Form validation messages
 $('form').prepend('<p class="error-message form-error">Please enter all required fields</p>');
-$('#name').after('<p class="error-message name-error">Please enter a name</p>');
-$('#mail').after('<p class="error-message mail-error">Please enter an email</p>');
-$('#other-title').after('<p class="error-message other-title-error">Please enter a job role</p>');
-$('.cc-info').after(`<div class="col-12 col">
-                        <p class="error-message cc-num-error">Please enter a valid credit card number</p>
-                        <p class="error-message zip-error">Please enter a valid zip code</p>
-                        <p class="error-message cvv-error">Please enter a valid cvv code</p>
-                     </div>`);
 $('.activities legend').append('<span class="error-message activities-error">- Please choose at least one activity -</span>');
-$('.exp-year-label').hide(); // hides CC expiration year label to help better display error messages
 $('.error-message').hide(); // Hide all error messages when page loads
 
 /*=============-=============-=============-=============
@@ -63,8 +54,7 @@ function toggleJobRole() {
   if(this.value === 'other') {
     $otherTitle.removeClass('is-hidden');
   } else {
-    $otherTitle.addClass('is-hidden').removeClass('error valid');
-    $('.other-title-error').hide(); // Hide error message when other option is not selected
+    $otherTitle.addClass('is-hidden').removeClass('error-bd valid').val('');
   }
 }
 
@@ -131,10 +121,9 @@ function computeActivitesCost(input) {
 function displayPaymentSection() {
   if (this.value !== 'credit card') {
     // When CC option is not selected, remove all CC inputs 'error' & 'valid' classes
-    $('#cc-num, #zip, #cvv').removeClass('error valid').val('');
-    $ccSection.find('.error-message').hide(); // hide error messages
+    $('#cc-num, #zip, #cvv').removeClass('error-bd valid').val('').prev('label').removeClass('error').find('span').remove('span');
     // If main form error message displayed, hide if no inputs have 'error' class
-    if ( $inputs.filter((i, input) => input.className === 'error').length === 0) {
+    if ( $inputs.filter((i, input) => input.className === 'error-bd').length === 0) {
       $('.form-error').hide();
     }
     if(this.value === 'paypal') {
@@ -177,27 +166,26 @@ function validateCC(id, value) {
 }
 
 // Adds a class of 'error' to highlight input & display's corresponding error message
-function showError(id, el, message) {
-  $(el).removeClass('valid').addClass('error');
-  $(`.${id}-error`).show();
-  if (message) {
-      $(`.${id}-error`).text(message);
+function showError(el, message) {
+  // const $label = $(el).prev('label');
+  if(!$(el).hasClass('error-bd')) {
+    $(el).removeClass('valid').addClass('error-bd').prev('label').addClass('error').append(message);
   }
 }
 
 // Removes 'error' class from input and hides error message
-function hideError(id, el) {
-  $(el).removeClass('error');
-  $(`.${id}-error`).hide();
+function hideError(el) {
+  $(el).removeClass('error-bd').prev('label').removeClass('error').find('span').remove('span');
+  // $(`.${id}-error`).hide();
 }
 
 // Adds a class of valid that highlights input and hides error message and class
-function addValidClass(id, el) {
-  hideError(id, el);
+function addValidClass(el) {
+  hideError(el);
   $(el).addClass('valid');
 }
 
-// Provides real-time validation error messages for text input fields
+// Provide real-time validation error messages for text input fields
 function validateInput(event) {
   const selectedInput = event.target;
   const id = selectedInput.id;
@@ -207,37 +195,52 @@ function validateInput(event) {
   let message = '';
 
   // Hide main form error message if no inputs have the class 'error' & at least 1 activity registered
-  if ( $inputs.filter((i, input) => input.className === 'error').length === 0 && activityRegistered()) {
+  if ( $inputs.filter((i, input) => input.className === 'error-bd').length === 0 && activityRegistered()) {
     $('.form-error').hide();
   }
-  // handles validation of name field & job role field (only if that option is selected)
-  if (id === 'name' || ( id === 'other-title' && !$otherTitle.hasClass('is-hidden')) ) {
-      value !== '' ? addValidClass(id, selectedInput) : showError(id, selectedInput);
+
+  // Handles validation of name field & job role field (only if that option is selected)
+  if (id === 'name') {
+      message = '<span>(please provide a name)</span>';
+      value !== '' ? addValidClass(selectedInput) : showError(selectedInput, message);
   }
-  // Email and CC validation
+  if ( id === 'other-title' && !$otherTitle.hasClass('is-hidden') ) {
+    value !== '' ? addValidClass(selectedInput) : showError(selectedInput);
+  }
+
+  // -- Keyup Events -- //
   if (type === 'keyup') {
+
       if (id === 'mail' && validateEmail(value)){
-          addValidClass(id, selectedInput);
+          addValidClass(selectedInput);
+          
       } else if ( id === 'cc-num' || id === 'zip' || id === 'cvv') {
           // First CC validation creates error message if user types more than maximum defined digits
           if (id === 'cc-num' && digits > 16 || id === 'zip' && digits > 5 || id === 'cvv' && digits > 3 ) {
-              showError(id, selectedInput);
+              showError(selectedInput);
           // Second CC validation uses validateCC function to make sure user input is a specific amount of numbers only
           } else if (validateCC(id, value)) {
-              addValidClass(id, selectedInput); 
+              addValidClass(selectedInput); 
           }
       }
+      
+  // -- Blur Events -- //
   } else if (type === 'blur') {
+
       if (id === 'mail' && value === '') {
-          message = 'Please enter an email address'; // Conditional email error message
-          showError('mail', selectedInput, message);            
+          message = '<span>(please enter an email address)</span>'; // Conditional email error message
+          showError(selectedInput, message);   
+
       } else if (id === 'mail' && !validateEmail(value)){
-          message = 'Please enter a valid email address'; // Conditional email error message
-          showError(id, selectedInput, message);
+          message = '<span>(please enter a valid email address)</span>'; // Conditional email error message
+          showError(selectedInput, message);
+
       } else if ( id === 'cc-num' || id === 'zip' || id === 'cvv') {
-          // Error message will show on blur event on invalid CC input
+          if (id === 'cc-num') message = '<span>(13 - 16 digits)</span>';
+          if (id === 'zip') message = '<span>(5 digits)</span>';
+          if (id === 'cvv') message = '<span>(3 digits)</span>';
           if(!validateCC(id, value)) {
-              showError(id, selectedInput);
+              showError(selectedInput, message);
           }
       }
   } 
@@ -260,19 +263,29 @@ function validateForm(event) {
       $('.form-error').show();
       if( !activityRegistered() ) $('.activities-error').show();
 
-      // Add the 'error' class to all inputs that do not have the class 'valid'
-      $inputs.filter((i, input) => {
-        const id = input.id;
-        if( id === 'cc-num' || id === 'zip' || id === 'cvv') {
-          // Do not include CC inputs if CC option not selected from Payment Info menu
-          return $paymentMenu.val() !== 'credit card' ? false : input.className !== 'valid';
-        } else if(id === 'other-title') {
-            // Do not include job role input if option is not selected or has class 'valid'
-            return input.className !== 'is-hidden' && input.className !== 'valid';
-        } else {
-          return input.className !== 'valid';
+      // Add the 'error-bd' class to all inputs that do not have the class 'valid'
+      $inputs.each(function() {
+        if(this.className !== 'valid') {
+          let message = '';
+          const id = this.id;
+
+          if( id === 'cc-num' || id === 'zip' || id === 'cvv') {
+            if ($paymentMenu.val() === 'credit card') {
+              if(id === 'cc-num') {
+                message = '<span>(enter a credit card number)</span>';
+                showError(this, message);
+              } else {
+                showError(this);
+              }
+            }
+          } else if ( id === 'other-title' && this.className !== 'is-hidden') {
+              showError(this, message);
+          } else if (id === 'mail' && this.value === '') {
+            message = '<span>(please enter an email address)</span>';
+            showError(this, message);            
+          }
         }
-      }).addClass('error');
+      });
   }
 }
 
@@ -293,7 +306,7 @@ $activitiesInputs.on('change', event => {
   // If activity error message displayed, hide when an activity is selected
   $('.activities-error').hide();
   // If main form error message displayed, hide if no inputs have 'error' class
-  if ( $inputs.filter((i, input) => input.className === 'error').length === 0) {
+  if ( $inputs.filter((i, input) => input.className === 'error-bd').length === 0) {
     $('.form-error').hide();
   }
 
